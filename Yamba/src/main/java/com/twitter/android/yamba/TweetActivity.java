8063b2b415123d5eb24bb2adb0d3ac08a7ac2ccc
Ken Jones <kennethj@twitter.com>
2013-11-21 18:10:04 -0800

    End of Day 2. Added ability to post Tweet via an AsyncTask.

--- Yamba/src/main/java/com/twitter/android/yamba/TweetActivity.java
+++ Yamba/src/main/java/com/twitter/android/yamba/TweetActivity.java
@@ -1,33 +1,42 @@
 package com.twitter.android.yamba;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.marakana.android.yamba.clientlib.*;
 
 public class TweetActivity extends ActionBarActivity
         implements View.OnClickListener {
 
     private static final String TAG = "TweetActivity";
 
-    EditText editTweet;
+    EditText mEditTweet;
+    YambaClient mYambaClient;
+    Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (BuildConfig.DEBUG) Log.v(TAG, "onCreate() invoked");
+
         setContentView(R.layout.activity_tweet);
-        editTweet = (EditText) findViewById(R.id.edit_tweet);
+        mEditTweet = (EditText) findViewById(R.id.edit_tweet);
         Button buttonTweet = (Button) findViewById(R.id.button_tweet);
         buttonTweet.setOnClickListener(this);
+
+        mToast = Toast.makeText(this, null, Toast.LENGTH_LONG);
+
+        mYambaClient = new YambaClient("student", "password");
     }
 
     @Override
@@ -37,13 +46,39 @@ public class TweetActivity extends ActionBarActivity
             case R.id.button_tweet:
                 // Tweet button clicked
                 if (BuildConfig.DEBUG) Log.v(TAG, "Tweet button clicked");
-                String msg = editTweet.getText().toString();
-                editTweet.setText("");
+                String msg = mEditTweet.getText().toString();
+                mEditTweet.setText("");
                 if (BuildConfig.DEBUG) Log.v(TAG, "User entered: " + msg);
+
+                if (!TextUtils.isEmpty(msg)) {
+                    new PostToTwitter().execute(msg);
+                }
                 break;
             default:
                 // We should never get here!
                 Log.w(TAG, "Unknown button clicked");
+        }
+    }
+
+   class PostToTwitter extends AsyncTask<String, Void, Integer> {
+        @Override
+        protected Integer doInBackground(String... params) {
+            int ret = R.string.post_tweet_fail;
+            if (params.length > 0) {
+                try {
+                    mYambaClient.postStatus(params[0]);
+                    ret = R.string.post_tweet_success;
+                } catch (YambaClientException e) {
+                    Log.w(TAG, "Failed to post message", e);
+                }
+            }
+            return ret;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            mToast.setText(result);
+            mToast.show();
         }
     }
 
